c603bddfa32ae7a79f55b1e9012e48f507d927ec
Julie Carlett <carlettju@gmail.com>
2023-07-15 21:47:20 -0300

    Update client-service.js

--- service/client-service.js
+++ service/client-service.js
@@ -1,40 +1,63 @@
-const criaNovaLinha = (nome,email) => {
-    const linhaNovoCliente = document.createElement("tr")
-    const conteudo = ` 
-<td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-                </td>
-                `
-    
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente;
-}
+// Define uma função de criação de nova linha
+const criaNovaLinha = (nome, email) => {
+  // Cria um novo elemento de linha de tabela
+  const linhaNovoCliente = document.createElement("tr");
 
-//tbody tada-tabela
-const tabela = document.querySelector("[data-tabela]")
+  // Define o conteúdo da linha com os dados passados e alguns elementos HTML estáticos
+  const conteudo = ` 
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+      <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+      </ul>
+    </td>
+    `;
 
+  // Adiciona o conteúdo à nova linha
+  linhaNovoCliente.innerHTML = conteudo;
 
+  // Retorna a nova linha
+  return linhaNovoCliente;
+};
 
-/******para cada modo exemplosemanaPassada criar dentro do http o http2 e se novo pedido http3 do mespassado porexemplo .. e por ai vai**/
-//constante para o request
-const http = new XMLHttpRequest()
+// Seleciona a tabela do documento pelo seu atributo data-tabela
+const tabela = document.querySelector("[data-tabela]");
 
-http.open("GET", "http://localhost:3000/profile") //abrindo comunicao
+// Define uma função para listar os clientes
+const listaClientes = () => {
+  // Cria uma nova Promise
+  const promise = new Promise((resolve, reject) => {
+    // Cria um novo XMLHttpRequest
+    const http = new XMLHttpRequest();
 
-http.send() //enviando
+    // Abre uma nova requisição GET para um endpoint específico
+    http.open("GET", "http://localhost:3000/profile");
 
-//carregar tomar uma acao quando o carregamento for concluido
-http.onload = () => {
-    const data = JSON.parse(http.response) //transforma o obj em js para reconhecer o forEach
-    data.forEach(element => {
-        tabela.appendChild(criaNovaLinha(element.nome, element.email))
+    // Envia a requisição
+    http.send();
 
-    })
+    // Define o que acontece quando a requisição é carregada
+    http.onload = () => {
+      // Se o status HTTP for 400 ou mais, rejeita a Promise com a resposta como motivo
+      if (http.status >= 400) {
+        reject(JSON.parse(http.response));
+      } else {
+        // Se o status HTTP for menor que 400, resolve a Promise com a resposta como valor
+        resolve(JSON.parse(http.response));
+      }
+    };
+  });
 
-}
+  // Retorna a Promise
+  return promise;
+};
 
+// Chama a função listaClientes
+listaClientes().then((data) => {
+  // Para cada elemento nos dados retornados, anexa uma nova linha à tabela
+  data.forEach((element) => {
+    tabela.appendChild(criaNovaLinha(element.nome, element.email));
+  });
+});
