129dfedd67ca8c8110197eb31ccd28893c360502
Julie Carlett <carlettju@gmail.com>
2023-07-17 22:41:22 -0300

    update

--- controller/cadastraClientes-controller.js
+++ controller/cadastraClientes-controller.js
@@ -1,15 +1,16 @@
-import { clienteService } from '../service/cliente-service.js'
+import { clienteService } from '../service/cliente-service.js';
 
-const formulario = document.querySelector('[data-form]')
+const formulario = document.querySelector('[data-form]');
 
+// Adiciona um ouvinte de evento de envio (submit) no formulário
+formulario.addEventListener('submit', (evento) => {
+  evento.preventDefault(); // Previne o comportamento padrão de recarregar a página ao enviar o formulário
 
-formulario.addEventListener('submit', (evento)=> { 
-  evento.preventDefault()
-  const nome = evento.target.querySelector('[data-nome]').value
-  const email = evento.target.querySelector('[data-email]').value
+  const nome = evento.target.querySelector('[data-nome]').value; // Obtém o valor do campo de nome do formulário
+  const email = evento.target.querySelector('[data-email]').value; // Obtém o valor do campo de email do formulário
 
-  clienteService.criaCliente(nome, email)
-  .then(()=> {
-    window.location.href = '../telas/cadastro_concluido.html'
-  })
-})
+  clienteService.criaCliente(nome, email) // Chama a função criaCliente do serviço de cliente, passando nome e email
+    .then(() => {
+      window.location.href = '../telas/cadastro_concluido.html'; // Redireciona para a página de cadastro concluído após a criação do cliente
+    });
+});
--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -1,39 +1,43 @@
-import { clienteService } from '../service/cliente-service.js'
+// Importando o módulo clienteService do arquivo cliente-service.js
+import { clienteService } from "../service/cliente-service.js";
 
-const criaNovaLinha = (nome, email, id) =>  { 
-  const linhaNovoCliente = document.createElement('tr')
+// Função para criar uma nova linha na tabela com dados de um cliente
+const criaNovaLinha = (nome, email, id) => {
+  const linhaNovoCliente = document.createElement("tr"); // Cria um elemento <tr> para representar a linha
   const conteudo = `
       <td class="td" data-td>${nome}</td>
-                  <td>${email}</td>
-                  <td>
-                      <ul class="tabela__botoes-controle">
-                          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                          <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                      </ul>
-                  </td> 
-                  `
-  linhaNovoCliente.innerHTML = conteudo
-  linhaNovoCliente.dataset.id = id
-  return linhaNovoCliente
-}
+      <td>${email}</td>
+      <td>
+          <ul class="tabela__botoes-controle">
+              <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+              <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+          </ul>
+      </td> 
+  `; // Define o conteúdo da linha com base nos dados do cliente
+  linhaNovoCliente.innerHTML = conteudo; // Define o conteúdo da linha criada
+  linhaNovoCliente.dataset.id = id; // Define o atributo data-id com o ID do cliente
+  return linhaNovoCliente; // Retorna a nova linha criada
+};
 
+const tabela = document.querySelector("[data-tabela]"); // Seleciona a tabela pelo atributo data-tabela
 
-const tabela = document.querySelector('[data-tabela]')
+// Adiciona um ouvinte de evento de clique na tabela
+tabela.addEventListener("click", (evento) => {
+  let ehBotaoDeDeleta =
+    evento.target.className === "botao-simples botao-simples--excluir"; // Verifica se o botão clicado é o botão de exclusão
+  if (ehBotaoDeDeleta) {
+    const linhaCliente = evento.target.closest("[data-id]"); // Encontra a linha do cliente mais próxima que contém o atributo data-id
+    let id = linhaCliente.dataset.id; // Obtém o ID do cliente da linha
+    clienteService.removeCliente(id).then(() => {
+      linhaCliente.remove(); // Remove a linha da tabela após a remoção do cliente
+    });
+  }
+});
 
-tabela.addEventListener('click', (evento)=> {
-    let ehBotaoDeDeleta = evento.target.className === 'botao-simples botao-simples--excluir'
-    if(ehBotaoDeDeleta){
-        const linhaCliente = evento.target.closest('[data-id]')
-        let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-        .then(()=>{
-            linhaCliente.remove()
-        })
-    }
-})
-
-clienteService.listaClientes()
-.then(data => {
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email, elemento.id))
-})})
+clienteService.listaClientes().then((data) => {
+  data.forEach((elemento) => {
+    tabela.appendChild(
+      criaNovaLinha(elemento.nome, elemento.email, elemento.id)
+    ); // Cria uma nova linha para cada cliente e adiciona à tabela
+  });
+});
